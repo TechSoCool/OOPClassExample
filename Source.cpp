12230def5f20f1a9c4642e002d0de3313eaa739f
DadWhoCodes <87879964+TechSoCool@users.noreply.github.com>
2022-05-17 19:40:17 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class SterlingClass {
+	public: 
+		void coolSayings() {
+			cout << "I love you Sam!" << endl;
+		}
+};
+
+int main()
+{
+	SterlingClass sterlingobjects;
+	sterlingobjects.coolSayings();
+	return 0; 
+}
